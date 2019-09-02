c19e7d45be7026fcbffd8cbdc67525100acd5ba8
Kaisin Li <kaisinli12@gmail.com>
2019-09-02 15:35:33 -0400

    removed view engine

--- app.js
+++ app.js
@@ -71,15 +71,10 @@ function onListening() {
 // create DB tables
 createTable()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // set up API routers
 const indexRouter = require('./routes/index');
